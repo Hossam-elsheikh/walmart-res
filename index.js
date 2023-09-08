5e51741bc71262f00f811d35049ba641b41033b2
Hossam-Elsheikh <hossam.m.elsheikh@gmail.com>
2023-09-09 00:26:25 +0300

    carousel

--- index.js
+++ index.js
@@ -2,4 +2,33 @@ import  {products} from './products.js'
 console.log(products);
 let carousel = document.getElementById('carousel');
 
+products.forEach(product => {
+    let item = document.createElement('div')
+    item.classList.add('item')
+    item.innerHTML = `
+    <i class="fa-regular fa-heart"></i>
+    <img src="${product.img}" alt="">
+    <p>${product.price}</p>
+    <p>${product.options}</p>
+    <p>${product.title}</p>
+    <button class="walBtn">Options</button>
+    `
+    carousel.appendChild(item)
+});
+
+let next = document.getElementById('arrow')
+let prev = document.getElementById('arrow2')
+
+next.onclick = () =>{
+        let currentMargin = +carousel.style.marginLeft.split('p')[0]
+        carousel.style.marginLeft = `${currentMargin - 600}px`
+    prev.style.display = 'block'
+}
+prev.onclick = () =>{
+    let currentMargin = +carousel.style.marginLeft.split('p')[0]
+    carousel.style.marginLeft = `${currentMargin + 600}px`
+    if(carousel.style.marginLeft === '0px'){
+        prev.style.display = 'none'
+    }
+}
 

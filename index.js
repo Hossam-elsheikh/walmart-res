431824ebac41b3c753b417a5ff1111e6729ecf11
Hossam-Elsheikh <hossam.m.elsheikh@gmail.com>
2023-09-09 03:02:38 +0300

    global

--- index.js
+++ index.js
@@ -1,43 +1,84 @@
-import  {products} from './products.js'
+import { products } from "./products.js";
 console.log(products);
-let carousel = document.getElementById('carousel');
-
-products.forEach(product => {
-    let item = document.createElement('div')
-    item.classList.add('item')
+let carousels = document.getElementsByClassName("caro");
+let next = document.getElementsByClassName("arrow");
+let prev = document.getElementsByClassName("arrow2");
+for (let i = 0; i < carousels.length; i++) {
+  products.forEach((product) => {
+    let item = document.createElement("div");
+    item.classList.add("item");
     item.innerHTML = `
-    <i class="fa-regular fa-heart"></i>
-    <img src="${product.img}" alt="">
-    <p>${product.price}</p>
-    <p>${product.options}</p>
-    <p>${product.title}</p>
-    <button class="walBtn">Options</button>
-    `
-    carousel.appendChild(item)
-});
-
-let next = document.getElementById('arrow')
-let prev = document.getElementById('arrow2')
-
-next.onclick = () =>{
-    let currentMargin = +carousel.style.marginLeft.split('p')[0]
-    if(currentMargin <= -1000){
-        next.style.display = 'none'
-    
-    } 
-    carousel.style.marginLeft = `${currentMargin - 600}px`
-    prev.style.display = 'block'
-  
-}
-prev.onclick = () =>{
-    let currentMargin = +carousel.style.marginLeft.split('p')[0]
-    if(currentMargin >= -1200){
-        next.style.display = 'block'
-    
-    } 
-    carousel.style.marginLeft = `${currentMargin + 600}px`
-    if(carousel.style.marginLeft === '0px'){
-        prev.style.display = 'none'
-    }
+        <i class="fa-regular fa-heart"></i>
+        <img src="${product.img}" alt="">
+        <p>${product.price}</p>
+        <p>${product.options}</p>
+        <p>${product.title}</p>
+        <button class="walBtn">Options</button>
+        `;
+    carousels[i].appendChild(item);
+  });
 }
 
+function slideNext(x, y) {
+  let currentMargin = +carousels[x].style.marginLeft.split("p")[0];
+  if (currentMargin <= y) {
+    next[x].style.display = "none";
+  }
+  carousels[x].style.marginLeft = `${currentMargin - 600}px`;
+  prev[x].style.display = "block";
+}
+
+function slidePrev(x, y) {
+  let currentMargin = +carousels[x].style.marginLeft.split("p")[0];
+  if (currentMargin >= y) {
+    next[x].style.display = "block";
+  }
+  carousels[x].style.marginLeft = `${currentMargin + 600}px`;
+  if (carousels[x].style.marginLeft === "0px") {
+    prev[x].style.display = "none";
+  }
+}
+
+next[0].onclick = () => {
+  slideNext(0, -1000);
+};
+
+prev[0].onclick = () => {
+  slidePrev(0, -1200);
+};
+
+next[1].onclick = () => {
+  slideNext(1, -2000);
+};
+
+prev[1].onclick = () => {
+  slidePrev(1, -2500);
+};
+
+// video play
+let video = document.getElementById("vid");
+let play = document.getElementsByClassName("fa-circle-play")[0];
+let mute = document.getElementsByClassName("fa-volume-low")[0];
+play.onclick = () => {
+  if (video.paused) {
+    video.play();
+    play.classList.remove("fa-circle-play");
+    play.classList.add("fa-circle-pause");
+  } else {
+    video.pause();
+    play.classList.remove("fa-circle-pause");
+    play.classList.add("fa-circle-play");
+  }
+};
+
+mute.onclick = () => {
+  if (!video.muted) {
+    video.muted = true;
+    mute.classList.remove("fa-volume-low");
+    mute.classList.add("fa-volume-xmark");
+  } else {
+    video.muted = false;
+    mute.classList.remove("fa-volume-xmark");
+    mute.classList.add("fa-volume-low");
+  }
+};
